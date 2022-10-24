71ffec46bf9ccf8b71d9542514a136ee8a4e15e5
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-24 18:23:50 +0200

    snack bonus COMPLETED

--- snack_Bonus/main.js
+++ snack_Bonus/main.js
@@ -3,3 +3,44 @@ Crea un array di oggetti che rappresentano delle persone.
 Ogni persona ha un nome, un cognome e un’età.
 Crea quindi un nuovo array inserendo, per ogni persona,
 una frase con il nome,cognome e l’indicazione se può guidare, in base all’età. */
+
+const peopleArray = [
+    {
+        nome: "Paolo",
+        cognome: "Forte",
+        eta: "16",
+    },
+    {
+        nome: "Mirko",
+        cognome: "Gallini",
+        eta: "36",
+    },
+    {
+        nome: "Franco",
+        cognome: "Sforzi",
+        eta: "21",
+    },
+    {
+        nome: "Gianluca",
+        cognome: "Paglierini",
+        eta: "19",
+    },
+    {
+        nome: "Carlo",
+        cognome: "Brembi",
+        eta: "17",
+    },
+]
+
+let newArray = []
+
+peopleArray.forEach(element => {
+    if (element.eta > 18) {
+        newArray.push(`${element.nome} ${element.cognome}, etò: ${element.eta}. Può guidare`)
+    } else {
+        newArray.push(`${element.nome} ${element.cognome}, etò: ${element.eta}. Non può guidare`)
+    }
+    //console.log(element.nome);
+});
+
+console.log(newArray);
