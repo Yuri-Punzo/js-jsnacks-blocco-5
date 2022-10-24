3be62b873be8b2c3fb7e50c258caf5fb3efa83b5
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-24 14:30:37 +0200

    snack_3 completed

--- snack_3/main.js
+++ snack_3/main.js
@@ -1,3 +1,17 @@
 /* Snack 3:
 Scrivi una funzione che accetti una stringa come argomento e la ritorni
 girata (es. Ciao -> oaiC) */
+
+function wordReverser(text) {
+    let reversedWord = "" // creo una costante vuota che poi diventerà la parola immessa scritta al contrario
+
+    for (let i = text.length; i > 0; i--) { // inizio il ciclo dall'ultima lettera per poi scorrere a ritroso
+        let char = text.charAt(i - 1); // si mette i-1 perché i parte a contare da 0, length invece parte da 1 !
+        //console.log(char);
+        reversedWord += char // rimonto la parola al contrario lettera per lettera
+    }
+    return reversedWord
+}
+
+
+console.log(wordReverser("Memento"));
