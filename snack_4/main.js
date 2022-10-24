c59fc6134b7cd1e6d173e4f98e1e46499d044ab9
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-24 17:37:32 +0200

    snack_4 completed

--- snack_4/main.js
+++ snack_4/main.js
@@ -3,6 +3,20 @@ Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi)
 prendendo alternativamente gli elementi da uno e dall’altro.
 es. [a,b,c], [1,2,3] → [a,1,b,2,c,3]. */
 
-const arrayOfLetters = ["a","b","c"]
+const arrayOfLetters = ["a", "b", "c"]
 
-const arrayOfNumbers = ["1","2","3"]
+const arrayOfNumbers = ["1", "2", "3"]
+
+const newArray = []
+
+const maxElementsInArray = 3
+
+arrayMerger()
+function arrayMerger() {
+    for (i = 0; i < maxElementsInArray; i++) {
+        newArray.push(arrayOfLetters[i]);
+        newArray.push(arrayOfNumbers[i]);
+    }
+}
+
+console.log(newArray);
